a67cad96ba646376a1901e1f49e944a3066572d0
katia-silva09 <katiajuliethsilvareyes09@gmail.com>
2026-09-22 20:48:01 -0600

    Actualizar el puerto predeterminado a 4000 en la configuración del servidor

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     instrument: ObserveInstrument,
   });
-  await app.listen(process.env.PORT ?? 3005);
+  await app.listen(process.env.PORT ?? 4000);
 }
 await bootstrap();
